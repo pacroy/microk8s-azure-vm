cf981c004f9cc6dfa401b8079287bae686796dea
Par <pacroy@gmail.com>
2022-02-05 21:52:07 +0700

    No uppercase in char7

--- locals.tf
+++ locals.tf
@@ -2,15 +2,16 @@ data "azurerm_resource_group" "main" {
   name = var.resource_group_name
 }
 
-resource "random_string" "instance_id" {
+resource "random_string" "char7" {
   length  = 7
   special = false
+  upper = false
 }
 
 locals {
   resource_group_name = var.resource_group_name
   location            = data.azurerm_resource_group.main.location
-  random_char7        = random_string.instance_id.result
+  random_char7        = random_string.char7.result
   suffix              = coalesce(var.suffix, local.random_char7)
 }
 
