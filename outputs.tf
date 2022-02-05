f6b5a63210fe73c0b4ba32e3e39140c9d01f5364
Par <pacroy@gmail.com>
2022-02-05 22:24:53 +0700

    Add output rg and suffix

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
+output "resource_group_name" {
+  value = local.resource_group_name
+}
+
+output "suffix" {
+  value = local.suffix
+}
+
 output "network_security_group" {
   value = azurerm_network_security_group.default
 }
