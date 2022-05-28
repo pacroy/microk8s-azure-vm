ad584d86d3a4c5f9f4db235b58a450f26dc38f76
Par <pacroy@gmail.com>
2022-05-28 09:54:17 +0700

    Add variable types

--- variables.tf
+++ variables.tf
@@ -1,42 +1,51 @@
 variable "resource_group_name" {
   description = "Resource group name to provision all resources"
+  type = string
 }
 
 variable "ip_address" {
   description = "IP address or range to allow access to the control ports of the VM. You may use `curl -s ipv4.icanhazip.com` to find your outbound public IP."
+  type = string
 }
 
 variable "location" {
   description = "The location to provision all resources. Omit to use the same location as the resource group."
+  type = string
   default     = ""
 }
 
 variable "email" {
   description = "Email address for receiving notifications from Let's Encrypt."
+  type = string
 }
 
 variable "suffix" {
   description = "Suffix of all resource names. Default is a 7-char random string."
+  type = string
   default     = ""
 }
 
 variable "admin_username" {
   description = "Admin username of the porvisioning VM."
+  type = string
   default     = "azureuser"
 }
 
 variable "address_space" {
   description = "Virtual netowrk address space in CIDR range."
+  type = string
   default     = "172.16.0.0/16"
 }
 
 variable "size" {
   description = "Virtual machine size."
+  type = string
   default     = "Standard_D2s_v5"
 }
 
 variable "allow_kubectl_from_azurecloud" {
   description = "Whether to add an inbound rule to allow kubectl from AzureCloud when connecting from GitHub Actions or Azure Pipelines."
+  type = bool
   default     = true
 }
 
